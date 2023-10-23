a12996cb3a129bab2e0e894a17b1c1fde325fcfc
scrawach <scrawach@gmail.com>
2023-10-23 14:11:18 +0300

    implemented resource file with colors settings

--- ui/components/base/line_menu.gd
+++ ui/components/base/line_menu.gd
@@ -5,9 +5,7 @@ signal button_pressed(tag: String, menu: LineMenu)
 
 @export var menu_items: Array[LineItem]
 
-var WHITE_RGB = Color(1, 1, 1)
-var LEFTMENU_DARK_BLUE_RGB = Color(0.4, 0.4862745098039216, 0.5568627450980392)
-var LEFTMENU_LIGHT_BLUE_RGB = Color(0.6, 0.6588235294117647, 0.7058823529411765)
+@export var colors: LineMenuColors 
 
 var line_items_painter: LineItemsPainter = LineItemsPainter.new()
 
@@ -20,21 +18,21 @@ func _ready() -> void:
 		item.item_pressed.connect(_on_item_pressed)
 
 func unselect_all() -> void:
-	line_items_painter.unselect(LEFTMENU_DARK_BLUE_RGB, menu_items)
+	line_items_painter.unselect(colors.unhover_color, menu_items)
 
 func _on_item_focus_entered(item: LineItem) -> void:
-	line_items_painter.hover(item, WHITE_RGB, menu_items)
+	line_items_painter.hover(item, colors.select_color, menu_items)
 
 func _on_item_focus_exited(item: LineItem) -> void:
-	line_items_painter.unhover(item, LEFTMENU_DARK_BLUE_RGB, menu_items)
+	line_items_painter.unhover(item, colors.unhover_color, menu_items)
 
 func _on_item_mouse_entered(item: LineItem) -> void:
-	line_items_painter.hover(item, LEFTMENU_LIGHT_BLUE_RGB, menu_items)
+	line_items_painter.hover(item, colors.hover_color, menu_items)
 
 func _on_item_mouse_exited(item: LineItem) -> void:
-	line_items_painter.unhover(item, LEFTMENU_DARK_BLUE_RGB, menu_items)
+	line_items_painter.unhover(item, colors.unhover_color, menu_items)
 
 func _on_item_pressed(item: LineItem) -> void:
-	line_items_painter.unselect(LEFTMENU_DARK_BLUE_RGB, menu_items)
-	line_items_painter.select(item, WHITE_RGB, menu_items)
+	line_items_painter.unselect(colors.unhover_color, menu_items)
+	line_items_painter.select(item, colors.select_color, menu_items)
 	button_pressed.emit(item.tag, self)
---
+++ ui/components/base/line_menu_colors.gd
@@ -0,0 +1,6 @@
+class_name LineMenuColors
+extends Resource
+
+@export var select_color: Color = Color(1, 1, 1)
+@export var hover_color: Color = Color(0.6, 0.6588235294117647, 0.7058823529411765)
+@export var unhover_color: Color = Color(0.4, 0.4862745098039216, 0.5568627450980392)
