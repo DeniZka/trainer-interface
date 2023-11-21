8f3d41c0f384b300a7d5a4219d8f8113eba93d48
scrawach <scrawach@gmail.com>
2023-11-21 14:54:48 +0300

    removed old things

--- Scripts/Combo_menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-var buttonPressedCounter = 0
-
-func _on_button_pressed():
-	$HBoxContainer/VBoxContainer/Users_info.add_user_line(buttonPressedCounter)
-	buttonPressedCounter = buttonPressedCounter + 1
-	if buttonPressedCounter == 4:
-		buttonPressedCounter = 0
