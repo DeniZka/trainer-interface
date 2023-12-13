5655a50183f99bc646c07031aadabe43308262a2
scrawach <scrawach@gmail.com>
2023-12-13 17:44:56 +0300

    implemented cross-platform file uploader and added it to menus

--- ui/windows/models/model_menu.gd
+++ ui/windows/models/model_menu.gd
@@ -14,12 +14,16 @@ signal closed()
 @onready var author_edit: LineEdit = %Author as LineEdit
 @onready var upload_date_edit: LineEdit = %"Upload Date" as LineEdit
 
+@onready var file_uploaded: FileUploader = %"File Uploader" as FileUploader
+@onready var file_name_label: Label = %"Loaded Filename" as Label
+
 func _ready() -> void:
 	control.saved_pressed.connect(_on_save_button_pressed)
 	control.delete_pressed.connect(_on_delete_button_pressed)
 	control.cancel_pressed.connect(_on_close_button_pressed)
 	close_button.pressed.connect(_on_close_button_pressed)
 	load_button.pressed.connect(_on_load_button_pressed)
+	file_uploaded.uploaded.connect(_on_file_uploaded)
 
 func _on_save_button_pressed() -> void:
 	pass
@@ -28,7 +32,10 @@ func _on_delete_button_pressed() -> void:
 	deleted.emit(null)
 
 func _on_load_button_pressed() -> void:
-	pass
+	file_uploaded.open()
 
 func _on_close_button_pressed() -> void:
 	closed.emit()
+
+func _on_file_uploaded(file_name: String, file_type: String, base64_data: String) -> void:
+	file_name_label.text = file_name
--- ui/windows/restarts/restart_menu.gd
+++ ui/windows/restarts/restart_menu.gd
@@ -13,12 +13,16 @@ signal closed()
 @onready var author_edit: LineEdit = %Author as LineEdit
 @onready var upload_date_edit: LineEdit = %"Upload Date" as LineEdit
 
+@onready var uploaded_filename: Label = %"Loaded Filename" as Label
+@onready var file_uploader: FileUploader = %"File Uploader" as FileUploader
+
 func _ready() -> void:
 	control.saved_pressed.connect(_on_save_button_pressed)
 	control.delete_pressed.connect(_on_delete_button_pressed)
 	control.cancel_pressed.connect(_on_close_button_pressed)
 	close_button.pressed.connect(_on_close_button_pressed)
 	load_button.pressed.connect(_on_load_button_pressed)
+	file_uploader.uploaded.connect(_on_file_uploaded)
 
 func _on_save_button_pressed() -> void:
 	pass
@@ -27,7 +31,10 @@ func _on_delete_button_pressed() -> void:
 	deleted.emit(null)
 
 func _on_load_button_pressed() -> void:
-	pass
+	file_uploader.open()
 
 func _on_close_button_pressed() -> void:
 	closed.emit()
+
+func _on_file_uploaded(file_name: String, file_type: String, base64_data: String) -> void:
+	uploaded_filename.text = file_name
