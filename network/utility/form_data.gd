7d6c955f68e1cfc661e4e92e590305c26bf25485
scrawach <scrawach@gmail.com>
2023-12-14 11:45:41 +0300

    added some documentation for form data class

--- network/utility/form_data.gd
+++ network/utility/form_data.gd
@@ -1,3 +1,23 @@
+## Multipart/form-data is a composite content type 
+## most commonly used for sending HTML forms with 
+## binary data using the POST method of the HTTP protocol.
+## about this format: wikipedia.org/wiki/Multipart/form-data
+
+## In example, after all, result like this:
+##
+## Content-Type: multipart/form-data; boundary=Asrf456BGe4h
+## --Asrf456BGe4h
+## Content-Disposition: form-data; name="DestAddress"
+##
+## Some message #1
+## --Asrf456BGe4h
+## Content-Disposition: form-data; name="AttachedFile1"; filename="photo.jpg"
+## Content-Type: image/jpeg
+##
+## [base64 for photo.jpg]
+## --Asrf456BGe4h--
+##
+
 class_name FormData
 extends RefCounted
 
@@ -9,42 +29,47 @@ var content: Array[FormDataItem]
 func _init() -> void:
 	headers.append("Content-Type: multipart/form-data; boundary=%s" % BOUNDARY)
 
+## Add specific header to multipart form data
 func add_header(header: String) -> FormData:
 	headers.append(header)
 	return self
 
+## Add text to form data body. You can transmit numbers, just use str() for it value
 func add_text(field_name: String, value: String) -> FormData:
 	var header = "Content-Disposition: form-data; name=\"%s\"" % field_name
 	content.append(FormDataItem.new([header], value))
 	return self
 
+## Add file to form data body
 func add_file(field_name: String, file_name: String, file_type: String, file_base64: String) -> FormData:
 	var disposition: String = "Content-Disposition: form-data; name=\"%s\"; filename=\"%s\"" % [field_name, file_name]
 	var content_type: String = "Content-Type: %s" % file_type
 	content.append(FormDataItem.new([disposition, content_type], file_base64))
 	return self
 
+## Convert all body content to string
 func body_as_string() -> String:
-	var result: PackedStringArray
+	var packed_body: PackedStringArray
 	var newline: String = "\r\n"
 	
 	for item in content:
-		result.append("--%s" % BOUNDARY)
-		result.append(newline)
+		packed_body.append("--%s" % BOUNDARY)
+		packed_body.append(newline)
 		for header in item.headers:
-			result.append(header)
-			result.append(newline)
-		result.append(newline)
-		result.append(item.content)
-		result.append(newline)
-	result.append("--%s--" % BOUNDARY)
-	result.append(newline)
+			packed_body.append(header)
+			packed_body.append(newline)
+		packed_body.append(newline)
+		packed_body.append(item.content)
+		packed_body.append(newline)
+	packed_body.append("--%s--" % BOUNDARY)
+	packed_body.append(newline)
 	
 	var newString: String
-	for line in result:
-		newString += line
+	for body_line in packed_body:
+		newString += body_line
 	return newString
 
+## Convert all body content to UTF8 bytes
 func body_as_raw() -> PackedByteArray:
 	return body_as_string().to_utf8_buffer()
 
