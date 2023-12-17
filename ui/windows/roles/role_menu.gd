994dfe2561fae2461c360e961e9aa21538028d2c
scrawach <scrawach@gmail.com>
2023-12-17 20:43:29 +0300

    upgrade role menu, reduced code

--- ui/windows/roles/role_menu.gd
+++ ui/windows/roles/role_menu.gd
@@ -1,52 +1,18 @@
 class_name RoleMenu
-extends Control
-
-signal saved(data: PersonRole)
-signal deleted(data: PersonRole)
-signal closed()
-
-@onready var save_button: Button = %"Save Button" as Button
-@onready var cancel_button: Button = %"Cancel Button" as Button
-@onready var delete_button: Button = %"Delete Button" as Button
-@onready var close_button: Button = %"Close Button" as Button
+extends BaseMenu
 
 @onready var rolename_edit: LineEdit = %Rolename as LineEdit
 
-var updated_role: PersonRole
+func _on_ready() -> void:
+	api = Api.roles
 
-var roles: JSONApi
-
-func _ready() -> void:
-	roles = Api.roles
-	save_button.pressed.connect(_on_save_button_pressed)
-	delete_button.pressed.connect(_on_delete_button_pressed)
-	cancel_button.pressed.connect(_on_close_button_pressed)
-	close_button.pressed.connect(_on_close_button_pressed)
-
-func open(person: PersonRole) -> void:
-	show()
-	updated_role = person
-	
-	if person != null:
-		rolename_edit.text = person.name
-
-func close() -> void:
-	hide()
-
-func _on_save_button_pressed() -> void:
-	var new_role: PersonRole = _create_person_role()
-	if updated_role != null:
-		roles.update(updated_role.id, new_role.serialize())
+func _on_update_view(data: PersonRole) -> void:
+	if data != null:
+		rolename_edit.text = data.name
 	else:
-		roles.create(new_role.serialize())
+		rolename_edit.text = ""
 
-func _on_delete_button_pressed() -> void:
-	deleted.emit(updated_role)
-
-func _on_close_button_pressed() -> void:
-	closed.emit()
-
-func _create_person_role() -> PersonRole:
+func _create_from_menu() -> PersonRole:
 	var role: PersonRole = PersonRole.new()
 	role.name = rolename_edit.text
 	return role
