4f3a7471611e3c241d55efb6a170fe0e5e547506
scrawach <scrawach@gmail.com>
2024-05-31 16:00:13 +0300

    added prebuilt setup from cmd line

---
+++ src/build_setup.gd
@@ -0,0 +1,31 @@
+extends SceneTree
+
+func _init() -> void:
+	var cmd_args: Dictionary = _cmd_arguments_to_dictionary()
+	_update_config_file_from(cmd_args)
+	quit()
+
+func _cmd_arguments_to_dictionary() -> Dictionary:
+	var args = {}
+	for arg in OS.get_cmdline_user_args():
+		if arg.find("=") > -1:
+			var values = arg.split("=")
+			var key = values[0].lstrip("--")
+			args[key] = values[1]
+	return args
+
+func _update_config_file_from(args: Dictionary) -> void:
+	const config_path: String = "res://config.cfg"
+	
+	var config = ConfigFile.new()
+	config.load(config_path)
+	
+	_update_matching_keys(config, args)
+	
+	config.save(config_path)
+
+func _update_matching_keys(config: ConfigFile, args: Dictionary) -> void:
+	for section in config.get_sections():
+		for key in config.get_section_keys(section):
+			if args.has(key):
+				config.set_value(section, key, args[key])
